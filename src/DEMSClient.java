b14d74099c047be3f0ebe80b0e4a4d7d2a5cf8bc
lilin7 <36003947+lilin7@users.noreply.github.com>
2019-05-27 00:41:06 -0400

    Update

--- src/DEMSClient.java
+++ src/DEMSClient.java
@@ -1,9 +1,18 @@
 import java.io.*;
 
+/**
+ * This class represents the object client for a distributed
+ * object of class DEMS, which implements the remote interface
+ * DEMSInterface.
+ */
 public class DEMSClient {
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		try {
+	         
+	      } 
+	      catch (Exception e) {
+	         System.out.println("Exception in DEMSClient: " + e);
+	      } 
 
 	}
 
