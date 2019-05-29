cc2025aa3164e7e2b025bea9e023c6e67a7b05a4
dianlutuabc <jeffrey95@163.com>
2019-05-28 22:44:39 -0400

    rmi frame work

--- src/server/DEMSImpl.java
+++ src/server/DEMSImpl.java
@@ -8,12 +8,42 @@ import java.rmi.server.*;
  */
 
 public class DEMSImpl extends UnicastRemoteObject implements DEMSInterface {
+	//hashmap
+	//hashmap(id, number)
+
 	public DEMSImpl() throws RemoteException {
 		super( );
 	}
 	
-	public void addEvent(String eventID, String eventType, int bookingCapacity) throws RemoteException {
-		
+	public void addEvent(String MID,String eventID, String eventType, int bookingCapacity) throws RemoteException {
+
 		
 	}
+
+	public void removeEvent(String MID , String eventID, String eventType) throws RemoteException{
+
+
+	}
+	public void listEventAvailability(String EventType) throws RemoteException{
+
+
+
+	}
+	public void bookEvent (String customerID,String eventID, String eventType) throws RemoteException{
+
+
+
+	}
+
+	public void getBookingSchedule(String customerID)throws RemoteException{
+
+
+
+	}
+	public void cancelEvent(String customerID,String eventID)throws RemoteException{
+
+
+
+	}
+
 }
--- src/server/DEMSInterface.java
+++ src/server/DEMSInterface.java
@@ -6,9 +6,15 @@ import java.rmi.*;
  */
 public interface DEMSInterface extends Remote {
 	
-	   public void addEvent(String eventID, String eventType, int bookingCapacity) 
-	      throws java.rmi.RemoteException;
+	   public void addEvent(String MID,String eventID, String eventType, int bookingCapacity) throws java.rmi.RemoteException;
 
+	   public void removeEvent(String MID , String eventID, String eventType) throws java.rmi.RemoteException;
 
-	   
+	   public void listEventAvailability(String EventType) throws java.rmi.RemoteException;
+
+	   public void bookEvent (String customerID,String eventID, String eventType)throws java.rmi.RemoteException;
+
+	   public void getBookingSchedule(String customerID) throws java.rmi.RemoteException;
+
+	   public void cancelEvent(String customerID,String eventID) throws java.rmi.RemoteException;
 }
