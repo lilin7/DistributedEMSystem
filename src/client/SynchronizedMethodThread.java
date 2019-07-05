15393b568430854aebe2affd65c79250e2223a97
dianlutuabc <jeffrey95@163.com>
2019-07-05 11:23:33 -0400

    CORBA multithreading for swap

--- src/client/SynchronizedMethodThread.java
+++ src/client/SynchronizedMethodThread.java
@@ -9,43 +9,102 @@ import java.util.Scanner;
 
 import server.DEMSInterface;
 
+
+
+import org.omg.CORBA.Any;
+import org.omg.CORBA.ORB;
+import org.omg.CosNaming.NamingContextExt;
+import org.omg.CosNaming.NamingContextExtHelper;
+
+import DEMS_CORBA.DEMSInterfaceCorba;
+import DEMS_CORBA.DEMSInterfaceCorbaHelper;
+
 public class SynchronizedMethodThread implements Runnable{
 
     private String customerID;
-    private String eventID;
-    private String eventType;
-    private DEMSInterface obj;
-    private int MTLportNumber = 1028;
+    private String oldEventID;
+    private String oldEventType;
+    private String newEventID;
+    private String newEventType;
+    private String[] args;
+    private DEMSInterfaceCorba obj;
+    // private DEMSInterface obj;
+    //private int MTLportNumber = 1028;
     private String ServerName = "MTLServer";
 
-    public SynchronizedMethodThread(String customerID, String eventID, String eventType){
+    public SynchronizedMethodThread(String customerID, String oldEventID, String oldEventType,String newEventID,String newEventType,String[] args){
         this.customerID = customerID;
-        this.eventID = eventID;
-        this.eventType = eventType;
+        this.oldEventID = oldEventID;
+        this.oldEventType = oldEventType;
+        this.newEventID = newEventID;
+        this.newEventType = newEventType;
+        this.args = args;
     }
 
 
     @Override
     public void run() {
         try{
+            Any any;
             ArrayList<String> returnMessage = new ArrayList<String>();
-            Registry registry = LocateRegistry.getRegistry(MTLportNumber);
-            obj = (DEMSInterface) registry.lookup(ServerName);
-            returnMessage = obj.bookEvent(customerID, eventID, eventType);
+            //Registry registry = LocateRegistry.getRegistry(MTLportNumber);
+            //obj = (DEMSInterface) registry.lookup(ServerName);
+            ORB orb = ORB.init(args, null);
+            org.omg.CORBA.Object objRef = orb.resolve_initial_references("NameService");
+            NamingContextExt ncRef = NamingContextExtHelper.narrow(objRef);
+            obj = (DEMSInterfaceCorba) DEMSInterfaceCorbaHelper.narrow(ncRef.resolve_str(ServerName));
 
-            if(returnMessage.get(0).equals("NoExist")) {
-                System.out.println("Fail. The event you attempt to book doesn't exist.");
+            any = obj.swapEvent(customerID, newEventID, newEventType ,oldEventID,oldEventType);
+            returnMessage = (ArrayList<String>)any.extract_Value();
+            String messageBookResult = returnMessage.get(0).trim();
+            String messageCancelResult = returnMessage.get(1).trim();
+            if (messageBookResult.equals("IdenticalEvents")) {
+                System.out.println("You are trying to swap two identical events, no action is taken.");
+            } else {
+                if(messageBookResult.equals("NoExist")) {
+                    System.out.println("Fail. The new event you attempt to book doesn't exist.");
 
-            }else if (returnMessage.get(0).equals("Full")){
-                System.out.println("customer: "+customerID+" Fail. This event is fully booked.");
+                }else if (messageBookResult.equals("Full")){
+                    System.out.println("Fail. This new event is fully booked.");
 
-            } else if (returnMessage.get(0).trim().equals("Success")) {
-                System.out.println(customerID+" have successfully booked a space in:  \n"
-                        + "Event type: " + eventType + "; Event ID: " + eventID + ".");
-            } else if (returnMessage.get(0).equals("NotUnique")) {
-                System.out.println("Fail. A customer can not book more than one event with the same event id and same event type.");
-            } else if (returnMessage.get(0).equals("Exceed3LimitInOtherCity")) {
-                System.out.println("Fail. A customer can only book at most 3 events from other cities overall in a month.");
+                } else if (messageBookResult.equals("NotUnique")) {
+                    System.out.println("Fail. You have booked the new event in the past. A customer can not book more than one event with the same event id and same event type.");
+
+                } else if (messageBookResult.equals("Exceed3LimitInOtherCity")) {
+                    System.out.println("Fail. A customer can only book at most 3 events from other cities overall in a month.");
+
+                } else if (messageBookResult.trim().equals("Success")) { // if book succeed
+                    if (messageCancelResult.equals("Success")) {
+                        System.out.println("Successfully swapped two events for customer " + customerID
+                                + ", old event ID: " + oldEventID + ", old event type: " + oldEventType
+                                + ", new event ID: " + newEventID + ", new event type: " + newEventType);
+
+                    } else if (messageCancelResult.equals("EventNotExist")) {
+                        System.out.println("The old event you want to cancel doesn't exist.");
+
+                    } else if (messageCancelResult.equals("CustomerNeverBooked")) {
+                        System.out.println("This customer never booked any event, can't cancel.");
+
+                    } else if (messageCancelResult.equals("ThisCustomerHasNotBookedThis")) {
+                        System.out.println("This customer has never booked the old event.");
+
+                    } else if (messageCancelResult.equals("Capacity Error")) {
+                        System.out.println("There is something wrong in the capacity record of the old event.");
+
+                    } else if (messageCancelResult.equals("SuccessButNoSuchCustomerIncBookingOtherCity")) {
+                        System.out.println("Successfully swapped. But for cancelling, NoSuchCustomerIncBookingOtherCity.");
+
+                    } else if (messageCancelResult.equals("SuccessButNoSuchMonthIncBookingOtherCity")) {
+                        System.out.println("Successfully swapped. But for cancelling, NoSuchMonthIncBookingOtherCity.");
+
+                    } else if (messageCancelResult.equals("SuccessButWrongNumberOfBookingIncBookingOtherCity")) {
+                        System.out.println("Successfully swapped. But for cancelling, WrongNumberOfBookingIncBookingOtherCity.");
+
+                    } else {
+                        System.out.println("wrong message received.");
+
+                    }
+                }
             }
 
         }catch (Exception e){
--- src/client/ThreadTest.java
+++ src/client/ThreadTest.java
@@ -3,11 +3,10 @@ package client;
 public class ThreadTest {
 
     public static void main(String[] args) {
-        Thread firstCustomer = new Thread(new SynchronizedMethodThread("MTLC1111","MTLE100519","Seminars"));
+        Thread firstCustomer = new Thread(new SynchronizedMethodThread("MTLC1111","MTLE100519","Seminars","MTLE100520","Seminars",args));
         firstCustomer.start();
-        Thread secondCustomer = new Thread(new SynchronizedMethodThread("MTLC2222","MTLE100519","Seminars"));
+        Thread secondCustomer = new Thread(new SynchronizedMethodThread("MTLC2222","MTLE100519","Seminars","MTLE100520","Seminars",args));
         secondCustomer.start();
-        Thread thirdCustomer = new Thread(new SynchronizedMethodThread("MTLC3333","MTLE100519","Seminars"));
-        thirdCustomer.start();
+
     }
 }
